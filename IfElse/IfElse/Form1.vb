ae3e6053a9e3a3742454c5ea1a82df2743c9c00c
idsc3101 <idsc3101@gmail.com>
2014-09-22 15:22:15 -0500

    Finished IfElse

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -5,26 +5,35 @@ Public Class Form1
     Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
         Dim num1, num2, largerNum As Double
 
-        ' Modify the program such that it does not crash 
-        ' if the user types incorrect input
-
         Dim st1 As String = txtFirstNum.Text
         Dim st2 As String = txtSecondNum.Text
+        Dim result As String
+        If Not (IsNumeric(st1) And IsNumeric(st2)) Then
+            MessageBox.Show("Please input numeric vaules only")
+            Return
+        End If
 
         num1 = CDbl(st1)
         num2 = CDbl(st2)
 
-        Dim result As String
-
+        If (num1 < 0) Or (num2 < 0) Then
+            MessageBox.Show("All inputs must be positive")
+            Return
+        End If
         If num1 > num2 Then
             result = "The larger number is " & num1
         ElseIf num2 > num1 Then
             result = "The larger number is " & num2
         Else
             result = "They are equal"
+
         End If
 
         txtResult.Text = result
+
     End Sub
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
